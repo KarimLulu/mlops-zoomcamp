ea7ee7b9fc591b50c0a2abd20f11817ed6a20942
Karim Lulu <karim.lulu@datarobot.com>
2022-06-05 17:13:11 +0200

    Add Homework 3

--- 03-orchestration/homework.py
+++ 03-orchestration/homework.py
@@ -1,18 +1,24 @@
 import datetime
-import pandas as pd
 from pathlib import Path
+import pickle
 
+import pandas as pd
+from prefect import flow, task, get_run_logger
+from prefect.task_runners import SequentialTaskRunner
+from prefect.deployments import DeploymentSpec
+from prefect.flow_runners import SubprocessFlowRunner
+from prefect.orion.schemas.schedules import CronSchedule
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
 
-from prefect import flow, task, get_run_logger
-from prefect.task_runners import SequentialTaskRunner
-
-
-BASE_PATH = Path().parent.resolve() / 'data'
+BASE_PATH = Path().parent.resolve()
+DATA_PATH = BASE_PATH / 'data'
+MODEL_PATH = BASE_PATH / 'models'
+MODEL_PATH.mkdir(exist_ok=True)
 BASE_FNAME = 'fhv_tripdata_{}-{:02d}.parquet'
+DATE_FMT = "%Y-%m-%d"
 
 
 @task
@@ -21,15 +27,19 @@ def read_data(path):
 
 
 def get_path(date):
-    return BASE_PATH / BASE_FNAME.format(date.year, date.month)
+    return DATA_PATH / BASE_FNAME.format(date.year, date.month)
 
 
-@task
-def get_paths(date=None):
+def get_date(date: str):
     if date is None:
         date = datetime.date.today()
     else:
-        date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+        date = datetime.datetime.strptime(date, DATE_FMT).date()
+    return date
+
+
+@task
+def get_paths(date):
     val_date = date.replace(day=1) - datetime.timedelta(days=1)
     train_date = val_date.replace(day=1) - datetime.timedelta(days=1)
     return get_path(train_date),  get_path(val_date)
@@ -56,7 +66,7 @@ def train_model(df, categorical):
     logger = get_run_logger()
     train_dicts = df[categorical].to_dict(orient='records')
     dv = DictVectorizer()
-    X_train = dv.fit_transform(train_dicts) 
+    X_train = dv.fit_transform(train_dicts)
     y_train = df.duration.values
 
     logger.info(f"The shape of X_train is {X_train.shape}")
@@ -81,8 +91,20 @@ def run_model(df, categorical, dv, lr):
     logger.info(f"The MSE of validation is: {mse}")
 
 
+def serialize_model(date, model, vectorizer):
+    logger = get_run_logger()
+    with (MODEL_PATH / f"model-{date}.bin").open("wb") as f:
+        pickle.dump(model, f)
+    vectorizer_path = MODEL_PATH / f"dv-{date}.pkl"
+    with vectorizer_path.open("wb") as f:
+        pickle.dump(vectorizer, f)
+    size = vectorizer_path.stat().st_size
+    logger.info(f"Vectorizer size {size:_} bytes")
+
+
 @flow(task_runner=SequentialTaskRunner())
-def main(date=None):
+def main(date='2021-08-15'):
+    date = get_date(date)
     train_path, val_path = get_paths(date).result()
     categorical = ['PUlocationID', 'DOlocationID']
 
@@ -95,6 +117,13 @@ def main(date=None):
     # train the model
     lr, dv = train_model(df_train_processed, categorical).result()
     run_model(df_val_processed, categorical, dv, lr)
+    serialize_model(date.strftime(DATE_FMT), lr, dv)
 
 
-main(date="2021-08-15")
+DeploymentSpec(
+    flow=main,
+    name="homework_model_training",
+    schedule=CronSchedule(cron='0 9 15 * *'),
+    flow_runner=SubprocessFlowRunner(),
+    tags=["ml", "homework", "duration-prediction"]
+)
