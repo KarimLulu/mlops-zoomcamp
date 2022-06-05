fd6f53a58175dd6729c87e2ebd71014c1e3ae678
Karim Lulu <karim.lulu@datarobot.com>
2022-06-05 15:48:44 +0200

    Homework 3 Q1-Q3

--- 03-orchestration/homework.py
+++ 03-orchestration/homework.py
@@ -1,67 +1,100 @@
+import datetime
 import pandas as pd
+from pathlib import Path
 
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-def read_data(path):
-    df = pd.read_parquet(path)
-    return df
 
+from prefect import flow, task, get_run_logger
+from prefect.task_runners import SequentialTaskRunner
+
+
+BASE_PATH = Path().parent.resolve() / 'data'
+BASE_FNAME = 'fhv_tripdata_{}-{:02d}.parquet'
+
+
+@task
+def read_data(path):
+    return pd.read_parquet(path)
+
+
+def get_path(date):
+    return BASE_PATH / BASE_FNAME.format(date.year, date.month)
+
+
+@task
+def get_paths(date=None):
+    if date is None:
+        date = datetime.date.today()
+    else:
+        date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+    val_date = date.replace(day=1) - datetime.timedelta(days=1)
+    train_date = val_date.replace(day=1) - datetime.timedelta(days=1)
+    return get_path(train_date),  get_path(val_date)
+
+
+@task
 def prepare_features(df, categorical, train=True):
+    logger = get_run_logger()
     df['duration'] = df.dropOff_datetime - df.pickup_datetime
     df['duration'] = df.duration.dt.total_seconds() / 60
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
 
     mean_duration = df.duration.mean()
     if train:
-        print(f"The mean duration of training is {mean_duration}")
+        logger.info(f"The mean duration of training is {mean_duration}")
     else:
-        print(f"The mean duration of validation is {mean_duration}")
-    
+        logger.info(f"The mean duration of validation is {mean_duration}")
     df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
     return df
 
-def train_model(df, categorical):
 
+@task
+def train_model(df, categorical):
+    logger = get_run_logger()
     train_dicts = df[categorical].to_dict(orient='records')
     dv = DictVectorizer()
     X_train = dv.fit_transform(train_dicts) 
     y_train = df.duration.values
 
-    print(f"The shape of X_train is {X_train.shape}")
-    print(f"The DictVectorizer has {len(dv.feature_names_)} features")
+    logger.info(f"The shape of X_train is {X_train.shape}")
+    logger.info(f"The DictVectorizer has {len(dv.feature_names_)} features")
 
     lr = LinearRegression()
     lr.fit(X_train, y_train)
     y_pred = lr.predict(X_train)
     mse = mean_squared_error(y_train, y_pred, squared=False)
-    print(f"The MSE of training is: {mse}")
+    logger.info(f"The MSE of training is: {mse}")
     return lr, dv
 
+
+@task
 def run_model(df, categorical, dv, lr):
+    logger = get_run_logger()
     val_dicts = df[categorical].to_dict(orient='records')
-    X_val = dv.transform(val_dicts) 
+    X_val = dv.transform(val_dicts)
     y_pred = lr.predict(X_val)
     y_val = df.duration.values
-
     mse = mean_squared_error(y_val, y_pred, squared=False)
-    print(f"The MSE of validation is: {mse}")
-    return
+    logger.info(f"The MSE of validation is: {mse}")
 
-def main(train_path: str = './data/fhv_tripdata_2021-01.parquet', 
-           val_path: str = './data/fhv_tripdata_2021-02.parquet'):
 
+@flow(task_runner=SequentialTaskRunner())
+def main(date=None):
+    train_path, val_path = get_paths(date).result()
     categorical = ['PUlocationID', 'DOlocationID']
 
     df_train = read_data(train_path)
     df_train_processed = prepare_features(df_train, categorical)
 
     df_val = read_data(val_path)
-    df_val_processed = prepare_features(df_val, categorical, False)
+    df_val_processed = prepare_features(df_val, categorical, train=False)
 
     # train the model
-    lr, dv = train_model(df_train_processed, categorical)
+    lr, dv = train_model(df_train_processed, categorical).result()
     run_model(df_val_processed, categorical, dv, lr)
 
-main()
+
+main(date="2021-08-15")
