f09943f3c24d8b77761703a35cfef0f62d689636
Karim Lulu <karim.lulu@datarobot.com>
2022-06-11 11:54:22 +0200

    Update streaming part

--- 04-deployment/web-service/predict.py
+++ 04-deployment/web-service/predict.py
@@ -2,21 +2,27 @@ import pickle
 
 from flask import Flask, request, jsonify
 
-with open('lin_reg.bin', 'rb') as f_in:
+
+FLASK_PORT = 9696
+MODEL_PATH = 'lin_reg.bin'
+
+
+with open(MODEL_PATH, 'rb') as f_in:
     (dv, model) = pickle.load(f_in)
 
 
 def prepare_features(ride):
-    features = {}
-    features['PU_DO'] = '%s_%s' % (ride['PULocationID'], ride['DOLocationID'])
-    features['trip_distance'] = ride['trip_distance']
+    features = {
+        'PU_DO': f"{ride['PULocationID']}_{ride['DOLocationID']}",
+        'trip_distance': ride['trip_distance']
+    }
     return features
 
 
 def predict(features):
     X = dv.transform(features)
     preds = model.predict(X)
-    return float(preds[0])
+    return preds[-1]
 
 
 app = Flask('duration-prediction')
@@ -27,14 +33,14 @@ def predict_endpoint():
     ride = request.get_json()
 
     features = prepare_features(ride)
-    pred = predict(features)
+    predicted_duration = predict(features)
 
     result = {
-        'duration': pred
+        'duration': predicted_duration
     }
 
     return jsonify(result)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=9696)
+    app.run(debug=True, host='0.0.0.0', port=FLASK_PORT)
--- 04-deployment/web-service/test.py
+++ 04-deployment/web-service/test.py
@@ -1,11 +1,14 @@
 import requests
 
+from predict import FLASK_PORT
+
+
 ride = {
     "PULocationID": 10,
     "DOLocationID": 50,
     "trip_distance": 40
 }
 
-url = 'http://localhost:9696/predict'
+url = f'http://localhost:{FLASK_PORT}/predict'
 response = requests.post(url, json=ride)
 print(response.json())
