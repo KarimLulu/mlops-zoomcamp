5cce27ca34ddf9c1888ef1f942c649eac856e972
Karim Lulu <karim.lulu@datarobot.com>
2022-06-25 00:46:56 +0200

    Submit homework

--- 04-deployment/batch/score_deploy.py
+++ 04-deployment/batch/score_deploy.py
@@ -1,16 +1,17 @@
-from prefect.deployments import Deployment
+from prefect.deployments import DeploymentSpec
 from prefect.orion.schemas.schedules import CronSchedule
-from score import ride_duration_prediction
+from prefect.flow_runners import SubprocessFlowRunner
 
-deployment = Deployment.build_from_flow(
-    flow=ride_duration_prediction,
+
+DeploymentSpec(
+    flow_location="score.py",
     name="ride_duration_prediction",
     parameters={
         "taxi_type": "green",
         "run_id": "e1efc53e9bd149078b0c12aeaa6365df",
     },
+    flow_storage="fb2163c7-c9ac-448b-b225-0fe9a9ae197a",
     schedule=CronSchedule(cron="0 3 2 * *"),
-    work_queue_name="ml",
+    flow_runner=SubprocessFlowRunner(),
+    tags=["ml"]
 )
-
-deployment.apply()
